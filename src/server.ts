d390ef8d179f6d637c88abedb9ceb64ff102d816
Luiz Hondo <lpmoh2@hotmail.com>
2026-06-13 16:10:36 -0300

    initial commit: minimal server funcionality

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express, { type Request, type Response } from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Servidor Express + TypeScript funcionando!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
